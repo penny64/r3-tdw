50b1265d78f5a3baa795d99478f178fd9ce0afd4
flags <jetstarforever@gmail.com>
2014-07-28 19:48:43 -0400

    Weapon searching.

--- tests/system_tests/system_ai.py
+++ tests/system_tests/system_ai.py
@@ -32,8 +32,10 @@ def _register(entity):
 	                         'in_cover': False,
 	                         'in_enemy_los': False,
 	                         'is_hungry': False,
-	                         'has_food': False},
-	                'weights': {}}
+	                         'has_food': False,
+	                         'sees_item_type_weapon': False},
+	                'weights': {'find_bandage': 4,
+	                            'find_weapon': 6}}
 	
 	entities.create_event(entity, 'logic')
 	entities.create_event(entity, 'logic_offline')
@@ -72,14 +74,6 @@ def register_human(entity):
                                   'in_cover',
                                   'in_enemy_los',
                                   'is_near')
-	_combat_brain.set_start_state(in_engagement=True,
-                                  is_near=False,
-                                  in_cover=False,
-                                  in_enemy_los=True,
-                                  has_ammo=False,
-                                  has_weapon=True,
-                                  weapon_armed=False,
-                                  weapon_loaded=False)
 	_combat_brain.set_goal_state(in_engagement=False)
 
 	_combat_actions = goapy.Action_List()
@@ -121,8 +115,6 @@ def register_human(entity):
 	_food_actions = goapy.Action_List()
 
 	_food_brain.set_action_list(_food_actions)
-	_food_brain.set_start_state(has_food=False,
-                                is_hungry=True)
 	_food_brain.set_goal_state(is_hungry=False)
 
 	_food_actions.add_condition('find_food', has_food=False)
@@ -133,6 +125,20 @@ def register_human(entity):
 	_food_actions.set_weight('eat_food', 10)
 	
 	_ai['brain'].add_planner(_food_brain)
+	
+	#Search
+	_weapon_seach_brain = goapy.Planner('has_weapon', 'sees_item_type_weapon')
+	_weapon_search_actions = goapy.Action_List()
+
+	_weapon_seach_brain.set_action_list(_weapon_search_actions)
+	_weapon_seach_brain.set_goal_state(has_weapon=True)
+
+	_weapon_search_actions.add_condition('find_weapon', has_weapon=False)
+	_weapon_search_actions.add_reaction('find_weapon', sees_item_type_weapon=True)
+	_weapon_search_actions.add_condition('get_weapon', sees_item_type_weapon=True)
+	_weapon_search_actions.add_reaction('get_weapon', has_weapon=True)
+	
+	_ai['brain'].add_planner(_weapon_seach_brain)
 	
 	entities.register_event(entity, 'logic', _human_logic)
 	entities.register_event(entity, 'logic_offline', _human_logic_offline)
