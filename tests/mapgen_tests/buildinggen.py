cd35685333ba475c44e7cb3ee82efecad1ac1b4a
Luke Martin <jetstarforever@gmail.com>
2014-08-07 17:27:58 -0400

    Buildinggen working.

--- tests/mapgen_tests/buildinggen.py
+++ tests/mapgen_tests/buildinggen.py
@@ -12,126 +12,67 @@ def _generate(width, height, facing, room_types, allow_tunnels=False):
 	_unclaimed_plots = []
 	_claimed_plots = {}
 	_claimed_plot_types = {}
-	_entrance_room_placed = False
 	
 	for y in range(height):
 		for x in range(width):
 			_unclaimed_plots.append((x, y))
 	
 	if facing == 'north':
-		_entrance_x = random.randint(0, width-1)
-		_entrance_y = 0
+		_plot_x = random.randint(0, width-1)
+		_plot_y = 0
 	
 	elif facing == 'south':
-		_entrance_x = random.randint(0, width-1)
-		_entrance_y = height-1
+		_plot_x = random.randint(0, width-1)
+		_plot_y = height-1
 	
 	elif facing == 'east':
-		_entrance_x = width-1
-		_entrance_y = random.randint(0, height-1)
+		_plot_x = width-1
+		_plot_y = random.randint(0, height-1)
 	
 	elif facing == 'west':
-		_entrance_x = 0
-		_entrance_y = random.randint(0, height-1)
+		_plot_x = 0
+		_plot_y = random.randint(0, height-1)
 	
-	while room_types:
-		room_type = room_types.pop(0)
+	for room_type in room_types:
+		_room = ROOM_TYPES[room_type]
+		_plot_map[_plot_y][_plot_x] = 1
+		_claimed_plots[(_plot_x, _plot_y)] = room_type
+		_plots_for_this_room = [(_plot_x, _plot_y)]
 		
-		_plots_needed = ROOM_TYPES[room_type]['plots'] - 1
-		_temp_unclaimed_plots = _unclaimed_plots[:]
+		if _room['plots'] == 1:
+			continue
 		
-		if _entrance_x > -1:
-			_x_plot = _entrance_x
-			_y_plot = _entrance_y
-			_entrance_x = -1
-			_entrance_y = -1
-		else:
-			while 1:
-				if not _temp_unclaimed_plots:
-					return -1
-				
-				_x_plot, _y_plot = _temp_unclaimed_plots.pop(random.randint(0, len(_temp_unclaimed_plots)-1))
-				_continue = False
-				_found_good_room = False
-				
-				if not _entrance_room_placed:
-					break
-				
-				for nn_x, nn_y in [(_x_plot+1, _y_plot), (_x_plot-1, _y_plot), (_x_plot, _y_plot+1), (_x_plot, _y_plot-1)]:
-					if (nn_x, nn_y) in _claimed_plots:
-						if _claimed_plots[(nn_x, nn_y)] in ROOM_TYPES[room_type]['avoid_rooms']:
-							_continue = True
-							
-							break
-						else:
-							_found_good_room = True
-				
-				if not _found_good_room and not allow_tunnels:
-					continue
-				
-				if not _continue:
-					break
-		
-		_claimed_plots[(_x_plot, _y_plot)] = room_type
-		
-		if room_type in _claimed_plot_types:
-			_claimed_plot_types[room_type].append((_x_plot, _y_plot))
-		else:
-			_claimed_plot_types[room_type] = [(_x_plot, _y_plot)]
-		
-		_check_positions = [(_x_plot+1, _y_plot), (_x_plot-1, _y_plot), (_x_plot, _y_plot+1), (_x_plot, _y_plot-1)]
-		_placed_this_iteration = [(_x_plot, _y_plot)]
-		
-		while _plots_needed > 0:
-			if not len(_check_positions):
-				room_types.insert(0, room_type)
-				
-				for x, y in _placed_this_iteration:
-					del _claimed_plots[(x, y)]
+		for _ in range(_room['plots']):
+			_available_next_plot_spaces = {}
+			
+			for _x, _y in _plots_for_this_room:
+				for n_x, n_y in [(_x+1, _y), (_x-1, _y), (_x, _y+1), (_x, _y-1)]:
+					if n_x < 0 or n_x >= _plot_map.shape[1] or n_y < 0 or n_y >= _plot_map.shape[0]:
+						continue
 					
-					_claimed_plot_types[room_type].remove((x, y))
-					_unclaimed_plots.append((x, y))
-				
-				return -1
-				break
-			
-			n_x, n_y = _check_positions.pop(random.randint(0, len(_check_positions)-1))
-			
-			#Check for OOB or claimed plot
-			if n_x < 0 or n_x >= width or n_y < 0 or n_y >= height or (n_x, n_y) in _claimed_plots:
-				continue
-			
-			#Check for neighbors of neighbors
-			_found_bad_room = False
-			_found_good_room = False
-			
-			for nn_x, nn_y in [(_x_plot+1, _y_plot), (_x_plot-1, _y_plot), (_x_plot, _y_plot+1), (_x_plot, _y_plot-1)]:
-				if (nn_x, nn_y) in _claimed_plots:
-					if _claimed_plots[(nn_x, nn_y)] in ROOM_TYPES[room_type]['avoid_rooms']:
-						_found_bad_room = True
-						
-						break
+					if _plot_map[n_y][n_x]:
+						continue
+					
+					_number_of_plots_around = available_plots_around(n_x, n_y, _plot_map)
+					
+					if _number_of_plots_around < _room['plots']-1:
+						continue
+					
+					if _number_of_plots_around in _available_next_plot_spaces:
+						_available_next_plot_spaces[_number_of_plots_around].append((n_x, n_y))
 					else:
-						_found_good_room = True
-
-			print _found_bad_room, _found_good_room
+						_available_next_plot_spaces[_number_of_plots_around] = [(n_x, n_y)]
 			
-			if _entrance_room_placed:
-				if _found_bad_room or (not _found_good_room and not allow_tunnels):
-					continue
+			#print _available_next_plot_spaces
+			#print
 			
-			_placed_this_iteration.append((n_x, n_y))
-			_claimed_plots[(n_x, n_y)] = room_type
-			_claimed_plot_types[room_type].append((n_x, n_y))
-			_unclaimed_plots.remove((n_x, n_y))
-			_plots_needed -= 1
-			_x_plot = n_x
-			_y_plot = n_y
+			if not _available_next_plot_spaces:
+				raise Exception('No plot spaces available.')
 			
-			if not _plots_needed:
-				print room_type
-		
-		_entrance_room_placed = True
+			_plot_x, _plot_y = random.choice(_available_next_plot_spaces[max(_available_next_plot_spaces.keys())])
+			_claimed_plots[(_plot_x, _plot_y)] = room_type
+			_plot_map[_plot_y][_plot_x] = 1
+			_plots_for_this_room.append((_plot_x, _plot_y))
 	
 	for y in range(height):
 		for x in range(width):
@@ -141,8 +82,27 @@ def _generate(width, height, facing, room_types, allow_tunnels=False):
 				print '.',
 		
 		print
+
+def available_plots_around(x, y, plot_map):
+	_plots = 1
+	_check_next = [(x, y)]
+	_temp_plot_map = plot_map.copy()
 	
-	return 1
+	while _check_next:
+		_x, _y = _check_next.pop(0)
+		
+		for n_x, n_y in [(_x+1, _y), (_x-1, _y), (_x, _y+1), (_x, _y-1)]:
+			if n_x < 0 or n_x >= plot_map.shape[1] or n_y < 0 or n_y >= plot_map.shape[0]:
+				continue
+			
+			if not _temp_plot_map[n_y][n_x]:
+				_check_next.append((n_x, n_y))
+		
+		if not _temp_plot_map[_y][_x]:
+			_temp_plot_map[_y][_x] = 1
+			_plots += 1
+	
+	return _plots
 
 def generate(width, height, facing, room_types, allow_tunnels=False):
 	while 1:
