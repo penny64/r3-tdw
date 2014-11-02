180596f4b797437ef8c06889473381868b4dd6bb
flags <jetstarforever@gmail.com>
2014-11-02 11:51:49 -0500

    About to branch

--- worldgen.py
+++ worldgen.py
@@ -4,13 +4,17 @@ import mapgen_arena
 import zones
 
 import logging
+import sys
 
 
 def generate():
 	logging.info('Generating world...')
 	
-	_width, _height, _node_grid, _node_sets, _weight_map, _tile_map, _solids, _fsl, _trees, _inside = mapgen_arena.generate(100, 100)
-	#_width, _height, _node_grid, _node_sets, _weight_map, _tile_map, _solids, _fsl, _trees, _inside = mapgen_swamp.generate(300, 300)
+	if '--arena' in sys.argv:
+		_width, _height, _node_grid, _node_sets, _weight_map, _tile_map, _solids, _fsl, _trees, _inside = mapgen_arena.generate(100, 100)
+	else:
+		_width, _height, _node_grid, _node_sets, _weight_map, _tile_map, _solids, _fsl, _trees, _inside = mapgen_swamp.generate(300, 300)
+	
 	_swamp_id = zones.create('swamps', _width, _height, _node_grid, _node_sets, _weight_map, _tile_map, _solids, _fsl, _trees, _inside)
 	
 	zones.populate_life(_swamp_id)
